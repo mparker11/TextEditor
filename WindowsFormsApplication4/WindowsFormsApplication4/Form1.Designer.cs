12da003935c2f0b0a3cad34ae8dd54590260cfbd
Martavis Parker <deon7388@gmail.com>
2011-06-02 11:32:31 -0400

    Simple Text Editor

---
+++ WindowsFormsApplication4/WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadFileButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.saveFileButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // loadFileButton
+            // 
+            this.loadFileButton.Location = new System.Drawing.Point(50, 40);
+            this.loadFileButton.Name = "loadFileButton";
+            this.loadFileButton.Size = new System.Drawing.Size(75, 23);
+            this.loadFileButton.TabIndex = 0;
+            this.loadFileButton.Text = "Load File";
+            this.loadFileButton.UseVisualStyleBackColor = true;
+            this.loadFileButton.Click += new System.EventHandler(this.loadFileButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 88);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(268, 166);
+            this.textBox1.TabIndex = 1;
+            // 
+            // saveFileButton
+            // 
+            this.saveFileButton.Location = new System.Drawing.Point(164, 40);
+            this.saveFileButton.Name = "saveFileButton";
+            this.saveFileButton.Size = new System.Drawing.Size(75, 23);
+            this.saveFileButton.TabIndex = 2;
+            this.saveFileButton.Text = "Save File";
+            this.saveFileButton.UseVisualStyleBackColor = true;
+            this.saveFileButton.Click += new System.EventHandler(this.saveFileButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(261, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Edit some Word Documents (.doc) and Text Files (.txt)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 266);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.saveFileButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.loadFileButton);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(300, 300);
+            this.Name = "Form1";
+            this.Text = "Simple Editor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadFileButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button saveFileButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApplication4/WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,141 @@
+﻿/* Creator: Martavis Parker
+ * 
+ * This sample program (WinForm) allows for a user to
+ * load a file, edit and save it. The coding is broken
+ * into a few methods. The methods created makes for 
+ * easier debugging, great organzation and erases 
+ * redundancy from parts that are similar to one
+ * another.
+ * 
+ *****May be upgraded some day to mimic Notepad :-)*****
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void loadFileButton_Click(object sender, EventArgs e)
+        {
+            loadFile();
+        }
+
+        private void saveFileButton_Click(object sender, EventArgs e)
+        {
+            saveFile();
+        }    
+
+
+//=================================== Dirty Work ==================================================
+        //Uses a dialog to open a file and load it to the textbox
+        private void loadFile()
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Text files (*.txt)|*.txt|Word 97-2003 Document (*.doc)|*.doc";
+ 
+            if (openFile.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (openFile.FileName.EndsWith(".txt") || openFile.FileName.EndsWith(".doc"))
+                {
+                    using (StreamReader strRead = new StreamReader(openFile.FileName))
+                    {
+                        String line;
+
+                        if (textBox1.Text.Length == 0)
+                        {
+                            while ((line = strRead.ReadLine()) != null)
+                            {
+                                textBox1.AppendText(line + "\n");
+                            }
+                        }
+                        else
+                        {
+                            textBox1.Text = "";
+                            while ((line = strRead.ReadLine()) != null)
+                            {
+                                textBox1.AppendText(line + "\n");
+                            }
+                        }
+                        strRead.Close();
+                    }   
+                }
+                else
+                {
+                    MessageBox.Show("Please choose a text file or Word document");
+                }
+            }   
+        }
+
+        //checks if the textbox is empty before saving
+        private void saveFile()
+        {
+            if (textBox1.Text.Length != 0)
+            {
+                saveIntoSet();
+            }     
+            else
+            {
+                switch(MessageBox.Show("Are you sure you would like to save an empty file?", "Warning", MessageBoxButtons.YesNo))
+                {
+                    case System.Windows.Forms.DialogResult.Yes:
+                        saveIntoSet();
+                        break;
+
+                    case System.Windows.Forms.DialogResult.No:
+                        break;
+                }  
+            } 
+        }
+
+        //uses hashset for intermediate step of writing to file - hashset used for speed vs list 
+        //(never know when a bigger file will be created)
+        private void saveIntoSet()
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            HashSet<string> set = new HashSet<string>();
+            set.Add(textBox1.Text);
+
+            saveFile.Filter = "Text files (*.txt)|*.txt|Word 97-2003 Document (*.doc)|*.doc";
+
+            if (saveFile.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (saveFile.FileName.EndsWith(".txt") || saveFile.FileName.EndsWith(".doc"))
+                {
+                    using (TextWriter strWrite = new StreamWriter(saveFile.FileName))
+                    {
+                        foreach (string line in set)
+                        {
+                            strWrite.Write(line);
+                        }
+                        strWrite.Close();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please choose a text file or Word document");
+                }
+            }
+
+            MessageBox.Show("File has been saved");
+
+            //Finally erase textbox text
+            if (textBox1.Text.Length != 0)
+            {
+                textBox1.Text = "";
+            }
+        }    
+    }
+}
